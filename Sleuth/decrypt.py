0057a61d6759d423e251392d91e70ecc188edcec
@pmikaelah <gianina.perezmikaelah@hcmut.edu.vn>
2023-09-02 20:07:45 +0700

    first commit

---
+++ Sleuth/decrypt.py
@@ -0,0 +1,9 @@
+print("Hello, Contosoville!")
+
+# This is a comment that won't be interpreted as a command
+
+# Associate variable town with the value "Contosoville"
+town = "Contosoville"
+
+# Print a message about the secret location
+print("The town I am looking for is " + town)
